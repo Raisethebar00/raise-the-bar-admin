bbf7cda5dec296cafd57d52d0027b7b48e9e6099
Sujeet Kumar <kumasuje@iu.edu>
2017-01-18 22:34:30 -0500

    Intial commit

---
+++ src/main/java/raise/the/bar/admin/controller/HomeController.java
@@ -0,0 +1,20 @@
+package raise.the.bar.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Sujeet on 1/18/17.
+ */
+
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value ="/")
+    public String home(){
+
+        return "index";
+    }
+
+}
