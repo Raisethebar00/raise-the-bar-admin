e00b3745d34977e0f759b08f53df73ac8783a5cb
Sujeet Kumar <kumasuje@iu.edu>
2017-03-20 15:36:30 -0400

    Adding test Endpoint to check heroku condition

--- src/test/java/raise/the/bar/admin/service/write/aws/AmazonAWSS3OperationTest.java
+++ src/test/java/raise/the/bar/admin/service/write/aws/AmazonAWSS3OperationTest.java
@@ -1,46 +1,46 @@
-package raise.the.bar.admin.service.write.aws;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.test.context.junit4.SpringRunner;
-import raise.the.bar.admin.write.aws.AmazonAWSS3Operation;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-/**
- * Created by Sujeet on 3/8/17.
- */
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class AmazonAWSS3OperationTest {
-
-    @Autowired
-    private AmazonAWSS3Operation amazonAWSS3Operation;
-
-    private File pic;
-
-
-    private Path path;
-
-    @Before
-    public void setUp(){
-
-        path = Paths.get("/Users/Manish/Pictures/003.JPG");
-    }
-
-    @Test
-    public void uploadFilesToS3_OK() throws IOException {
-
-        amazonAWSS3Operation.uploadFilesToS3(new MockMultipartFile("new Name", Files.readAllBytes(path)),"testbucket");
-    }
-
-}
+//package raise.the.bar.admin.service.write.aws;
+//
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.mock.web.MockMultipartFile;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import raise.the.bar.admin.write.aws.AmazonAWSS3Operation;
+//
+//import java.io.File;
+//import java.io.IOException;
+//import java.nio.file.Files;
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
+//
+///**
+// * Created by Sujeet on 3/8/17.
+// */
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class AmazonAWSS3OperationTest {
+//
+//    @Autowired
+//    private AmazonAWSS3Operation amazonAWSS3Operation;
+//
+//    private File pic;
+//
+//
+//    private Path path;
+//
+//    @Before
+//    public void setUp(){
+//
+//        path = Paths.get("/Users/Manish/Pictures/003.JPG");
+//    }
+//
+//    @Test
+//    public void uploadFilesToS3_OK() throws IOException {
+//
+//        amazonAWSS3Operation.uploadFilesToS3(new MockMultipartFile("new Name", Files.readAllBytes(path)),"testbucket");
+//    }
+//
+//}
--- src/test/java/raise/the/bar/admin/service/write/serviceImpl/ItemServiceImpllTest.java
+++ src/test/java/raise/the/bar/admin/service/write/serviceImpl/ItemServiceImpllTest.java
@@ -1,50 +1,50 @@
-package raise.the.bar.admin.service.write.serviceImpl;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import raise.the.bar.admin.model.BarTimingModel;
-import raise.the.bar.admin.model.ItemModel;
-import raise.the.bar.admin.write.service.ItemService;
-import raise.the.bar.admin.write.serviceImpl.BarTimingServiceImpl;
-
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.sql.Time;
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- * Created by Sujeet on 3/16/17.
- */
-
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class ItemServiceImpllTest {
-
-    @Autowired
-    private ItemService itemService;
-
-    private ItemModel itemModel;
-
-    @Before
-    public void setUp(){
-
-        itemModel = new ItemModel();
-        itemModel.setBarId(5);
-        itemModel.setName("new ");
-        itemModel.setPrice(new BigDecimal(89));
-        itemModel.setQuantity("67 ml");
-    }
-
-    @Test
-    public void edit_OK() throws IOException {
-
-
-        System.out.println(itemService.edit(itemModel));
-    }
-}
+//package raise.the.bar.admin.service.write.serviceImpl;
+//
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import raise.the.bar.admin.model.BarTimingModel;
+//import raise.the.bar.admin.model.ItemModel;
+//import raise.the.bar.admin.write.service.ItemService;
+//import raise.the.bar.admin.write.serviceImpl.BarTimingServiceImpl;
+//
+//import java.io.IOException;
+//import java.math.BigDecimal;
+//import java.sql.Time;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+///**
+// * Created by Sujeet on 3/16/17.
+// */
+//
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class ItemServiceImpllTest {
+//
+//    @Autowired
+//    private ItemService itemService;
+//
+//    private ItemModel itemModel;
+//
+//    @Before
+//    public void setUp(){
+//
+//        itemModel = new ItemModel();
+//        itemModel.setBarId(5);
+//        itemModel.setName("new ");
+//        itemModel.setPrice(new BigDecimal(89));
+//        itemModel.setQuantity("67 ml");
+//    }
+//
+//    @Test
+//    public void edit_OK() throws IOException {
+//
+//
+//        System.out.println(itemService.edit(itemModel));
+//    }
+//}
